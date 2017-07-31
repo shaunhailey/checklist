0ee1a4cdb885938b769c00f1d7f3ac18174ee436
shaunhailey <ediblelysol@gmail.com>
2017-07-31 16:01:13 -0400

    working

--- server.js
+++ server.js
@@ -3,32 +3,54 @@ const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const session = require('express-session')
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressValidator())
-
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  })
+)
 app.engine('mst', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mst')
 
-let myList = []
+//gets the form input from the browser
 app.get('/', function(req, res) {
-  res.render('index', { todos: myList })
+  const todoList = req.session.todoList || []
+  const templateData = {
+    uncompleted: todoList.filter(todo => !todo.completed),
+    completed: todoList.filter(todo => todo.completed)
+  }
+  res.render('index', templateData)
 })
 
-app.post('/', (req, res) => {
-  req.checkBody('newItem', 'Need an item to do!').notEmpty()
-  const errors = req.validationErrors()
-  if (errors) {
-    res.render('index', { errors })
-  } else {
-    myList.push(req.body.newItem)
-    res.render('index', {
-      todos: myList
-    })
+//posting the data to the browser for new todo
+app.post('/addTodo', (req, res) => {
+  const todoList = req.session.todoList || []
+  const descriptionForNewTodo = req.body.description
+
+  todoList.push({ id: todoList.length + 1, completed: false, description: descriptionForNewTodo })
+  req.session.todoList = todoList
+  res.redirect('/')
+})
+
+//posting completed todos to browser
+app.post('/markComplete', (req, res) => {
+  const todoList = req.session.todoList || []
+  const id = parseInt(req.body.id)
+  const todo = todoList.find(todo => todo.id === id)
+
+  if (todo) {
+    todo.completed = true
+    req.session.todoList = todoList
   }
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
